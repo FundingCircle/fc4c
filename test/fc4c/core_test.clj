d67592274ccf2a7df8ec5c199c9ec1348bdcf8db
Avi Flax <avi.flax@fundingcircle.com>
2018-04-02 18:05:34 -0400

    Remove shutdown-agents hack
    Doesn’t seem to be needed anymore

--- test/fc4c/core_test.clj
+++ test/fc4c/core_test.clj
@@ -9,8 +9,3 @@
 (deftest round-to-closest (check `rc/round-to-closest))
 (deftest snap-coords (check `rc/snap-coords))
 (deftest shrink (check `rc/shrink 300))
-
-;; This is a fake test that exists just to shut down the thread pools that back agents. None of the
-;; code in this project uses agents, but I guess maybe one of the libraries does. Not sure which.
-;; For more see https://groups.google.com/d/msg/clojure/zNh8zQbTQq4/VUjl75dwAgAJ
-(deftest foo (do (shutdown-agents) (ct/is true)))
